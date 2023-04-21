1a9b132c71eb95490a76bd1ea36a2e6b741903b3
Swetha Reddy <swethakiranmv@gmail.com>
2023-04-21 22:35:53 +0530

    random

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
 import React from "react";
 import "./App.css";
-import { useState,useEffect, } from "react";
+import { useState, useEffect, } from "react";
 
 
 const App = () => {
-  const [quote,setNewquote]=useState("");
+  const [quote, setNewquote] = useState("");
   //console.log(quote);
 
   const fetchData = async () => {
     const response = await fetch(`https://api.quotable.io/random`);
     const newData = await response.json();
     setNewquote(newData);
-};
- 
+  };
+
   useEffect(() => {
 
-  fetchData();
-    
-  },[]);
+    fetchData();
+
+  }, []);
 
 
-    return (
-      <div id="main">
-       <div class="card-header">
-      Inspiring Quotes!!
-  </div>
-  <div class="card-body">
-      <p class="card-text"> {quote.content}</p>
-    <h5 class="card-title"> {quote.author}</h5>
-    <button type="button" class="btn btn-success" onClick={fetchData}>Generate New Quote</button>
-  </div>
- 
-
-
-</div>
-          
-               
- 
-    );
+  return (
+    <div id="main">
+      <div class="card-header">
+        Inspiring Quotes!!
+      </div>
+      <div class="card-body">
+        <p class="card-text"> {quote.content}</p>
+        <h5 class="card-title"> {quote.author}</h5>
+        <button type="button" class="btn btn-success" onClick={fetchData}>Generate New Quote</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
